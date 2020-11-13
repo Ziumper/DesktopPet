9d47eb66ae0c8050375b91bd2ecb250f525f5ebe
Tomasz Komoszeski <tomasz.komoszeski@gmail.com>
2020-11-13 23:33:47 +0100

    Example solution from internet

---
+++ Assets/Scripts/TestClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClick : MonoBehaviour
+{
+    public GameObject SomeRandomGameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TestingClicking()
+    {
+        SomeRandomGameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
